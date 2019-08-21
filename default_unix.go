0e44f4d36af062b8b540b850ff9cd667cc6ddcc2
guhao022 <378999587@qq.com>
2019-08-21 16:16:41 +0800

    fix

--- default_unix.go
+++ default_unix.go
@@ -7,13 +7,13 @@ import (
 )
 
 const DEFAULT_STORE = `/tmp`
-//var godaemon = flag.Bool("d", true, "run app as a daemon with -d=true")
 
 func pull(gitRoot string) error {
 	if gitRoot == "" {
 		return fmt.Errorf("%s", "gitRoot is empty!!")
 	}
 
+	// 线上环境，强制覆盖
 	if _, err := runCommand(gitRoot, "/bin/sh", "-c", "git fetch --all && git reset --hard origin/master && git pull"); err != nil {
 		return err
 	}
@@ -21,23 +21,14 @@ func pull(gitRoot string) error {
 	return nil
 }
 
-/*func init() {
-
-	flag.Parse()
-
-	if *godaemon {
-		args := os.Args[1:]
-		i := 0
-		for ; i < len(args); i++ {
-			if args[i] == "-d=true" {
-				args[i] = "-d=false"
-				break
-			}
-		}
-		cmd := exec.Command(os.Args[0], args...)
-		cmd.Start()
-
-		fmt.Println("[PID]", cmd.Process.Pid)
-		os.Exit(0)
+func push(gitRoot string) error {
+	if gitRoot == "" {
+		return fmt.Errorf("%s", "gitRoot is empty!!")
 	}
-}*/
+
+	if _, err := runCommand(gitRoot, "/bin/sh", "-c", "git push -u origin master -f"); err != nil {
+		return err
+	}
+
+	return nil
+}
--- default_win.go
+++ default_win.go
@@ -11,7 +11,20 @@ func pull(gitRoot string) error {
 		return fmt.Errorf("%s", "gitRoot is empty!!")
 	}
 
+	// 开发环境，谨慎拉取
 	if _, err := runCommand(gitRoot, "git", "pull"); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func push(gitRoot string) error {
+	if gitRoot == "" {
+		return fmt.Errorf("%s", "gitRoot is empty!!")
+	}
+
+	if _, err := runCommand(gitRoot, "/bin/sh", "-c", "git push -u origin master -f"); err != nil {
 		return err
 	}
 
